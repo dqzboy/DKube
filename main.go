c15093dc09cd004af59fb0d61f14b2611c6c874a
dqzboy <42825450+dqzboy@users.noreply.github.com>
2022-09-25 21:13:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,10 +9,9 @@ import (
 
 func main() {
 	//初始化k8s client
-	service.K8s.Init() //可以使用service.K8s.ClientSet 跨包调用
+	service.K8s.Init()
 	//初始化Gin对象
 	r := gin.Default()
-	//跨包调用router的方法，初始化路由规则
 	controller.Router.InitApiRouter(r)
 	//启动gin程序
 	r.Run(config.ListenAddr)
